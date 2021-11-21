21ac3be0190203858d0ff621ab8f7b28207328d7
Ashutosh Sathe <2ashutoshbs@gmail.com>
2021-11-21 19:54:29 +0530

    Prettifying plots

--- evaluation/compute_prob_matrix.py
+++ evaluation/compute_prob_matrix.py
@@ -5,15 +5,18 @@ src_path = '../../results_32/bird_{psi}/DoodlerGAN_all/part_gen/'
 partlist = 'generated*.txt'
 allparts = 'all_parts.txt'
 
-for psi in [0.5, 0.6, 0.7, 0.8, 0.9]:
+max_steps = 8
+
+for psi in [0.5, 0.6, 0.7, 0.8, 0.9, 1.0]:
     with open(src_path.format(psi=psi) + allparts, 'r') as f:
-        curr_parts_list = f.readlines()
+        curr_parts_list = [line.strip() for line in f.readlines()]
+    curr_parts_list.remove('none')
     probs = np.zeros((len(curr_parts_list), len(curr_parts_list)))
     num_parts_used = np.zeros(len(curr_parts_list)+1)
-    step_part_distribution = np.zeros((10, len(curr_parts_list)))
+    step_part_distribution = np.zeros((max_steps, len(curr_parts_list)))
     for partorder_file in glob.glob(src_path.format(psi=psi) + partlist):
         with open(partorder_file, 'r') as f:
-            partorder = f.readlines()
+            partorder = [line.strip() for line in f.readlines()]
         num_parts_used[len(partorder)] += 1
         if len(partorder) == 0:
             continue
@@ -32,13 +35,19 @@ for psi in [0.5, 0.6, 0.7, 0.8, 0.9]:
     print(probs)
     print(step_part_distribution)
     plt.clf()
-    plt.matshow(probs)
+    plt.matshow(probs, cmap='Pastel2')
+    for i in range(len(probs)):
+        for j in range(len(probs[0])):
+            plt.text(j, i, f'{probs[i, j]:.2f}', ha='center', va='center')
     plt.xticks(range(len(curr_parts_list)), labels=curr_parts_list, rotation='vertical')
     plt.yticks(range(len(curr_parts_list)), labels=curr_parts_list)
     plt.savefig(f'probs_{psi}.pdf', bbox_inches='tight', dpi=300)
     plt.clf()
-    plt.matshow(step_part_distribution.T)
-    plt.xticks(range(10), labels=[f'Step {i+1}' for i in range(10)], rotation='vertical')
+    plt.matshow(step_part_distribution.T, cmap='Wistia')
+    for i in range(len(step_part_distribution.T)):
+        for j in range(len(step_part_distribution.T[0])):
+            plt.text(j, i, f'{step_part_distribution.T[i, j]:.2f}', ha='center', va='center')
+    plt.xticks(range(max_steps), labels=[f'Step {i+1}' for i in range(max_steps)], rotation='vertical')
     plt.yticks(range(len(curr_parts_list)), labels=curr_parts_list)
     plt.savefig(f'stepwise_part_distribution_{psi}.pdf', bbox_inches='tight', dpi=300)
     print(64*'-')
